c00ced9d28b439fa100ae099951a2a136f123faf
jiyooncha <wendycha@naver.com>
2025-12-17 13:08:39 +0900

    heart, actor wip

--- curtaincall-front/src/components/common/icon/Heart.vue
+++ curtaincall-front/src/components/common/icon/Heart.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>
-
+    <div class="box" @click="heartClicked">
+        <div><i :class="icon"></i> 40</div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+
+const icon = ref("bi bi-heart")
+const heartClicked = function() {
+    if (icon.value === "bi bi-heart") {
+        icon.value = "bi bi-heart-fill"
+    } else {
+        icon.value = "bi bi-heart"
+    }
+}
 
 </script>
 
 <style scoped>
+    .box {
+        border: 1px #800000 solid;
+        border-radius: 10px;
+        color : #800000;
+        font-size: 16px;
+        font-weight: bold;
+        width: 60px;
+        height: 35px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
 </style>
