dced9fe55631d21a878018c16e1ac6402eb21c53
jiwon <jiwonseo199@gmail.com>
2025-12-22 12:33:20 +0900

    연결

--- curtaincall-front/src/components/common/CardItem.vue
+++ curtaincall-front/src/components/common/CardItem.vue
@@ -1,53 +1,66 @@
 <template>
-    <div class="poster">
+    <div class="poster" @click="goDetail">
         <div class="card">
             <div class="img-box">
                 <img :src="`/${item.image}`">
-                 <!-- <img :src="musical.이미지........"> -->
+                <!-- <img :src="musical.이미지........"> -->
             </div>
-            <div class="title basic-text">{{item.title}}</div>
+            <div class="title basic-text">{{ item.title }}</div>
         </div>
     </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
+
 const props = defineProps({
     item: Object
 })
 console.log(props.item)
+
+const router = useRouter()
+
+const goDetail = () => {
+  router.push(`/musical/${props.item.musicalId}`)
+}
+
 </script>
 
 <style scoped>
 .img-box {
     width: 100%;
-    aspect-ratio: 3/4; /*비율*/
+    aspect-ratio: 3/4;
+    /*비율*/
     /* border-radius: 12px; */
 }
+
 img {
     width: 100%;
     height: 100%;
-    object-fit:cover;
+    object-fit: cover;
 }
+
 .poster {
     display: flex;
     flex-direction: column;
     align-items: center;
 }
 
- .card {
-  background: #fff;
-  border-radius: 12px;
-  border-color: white;
-  overflow: hidden;
-  cursor: pointer;
-  transition: transform 0.2s, box-shadow 0.2s;
-  text-align: center;
+.card {
+    background: #fff;
+    border-radius: 12px;
+    border-color: white;
+    overflow: hidden;
+    cursor: pointer;
+    transition: transform 0.2s, box-shadow 0.2s;
+    text-align: center;
 }
 
 .card:hover {
-  transform: translateY(-4px);
-  box-shadow: 0 8px 20px rgba(0,0,0,0.12);
+    transform: translateY(-4px);
+    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.12);
 }
+
 /* 
 .title {
   padding: 5px;
@@ -55,6 +68,4 @@ img {
   font-weight: 600;
   text-align: center;
 } */
-
-
 </style>
