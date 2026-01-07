62a37378e5cac3b88036c68b898cdc56f6d082d2
차지윤 <wendycha@naver.com>
2026-01-07 10:14:51 +0900

    cors changed

--- CurtainCall/src/main/java/com/ssafy/curtaincall/SecurityConfig.java
+++ CurtainCall/src/main/java/com/ssafy/curtaincall/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://curtaincall2025.vercel.app"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
@@ -57,7 +57,6 @@ public class SecurityConfig {
         return source;
     }
 
-    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
